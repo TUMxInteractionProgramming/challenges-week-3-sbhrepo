6b2cd6b0d6d005079c1345d084c09fd2f90c7cc8
sbhrepo <sbhrepo@users.noreply.github.com>
2018-09-21 16:54:48 -0400

    update week 3

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
 /*console.log('app is alive');*/
 
 function switchChannel(channelName) {
-    "use strict";
     console.log('Tuning into channel' + channelName);
-    document.getElementById('chat-channel-name').innerHTML = channelName + ' <small>by <a style="color: white"; href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a></small><img id="star-image" onclick="toggleStar();" src="http://ip.lfe.mw.tum.de/sections/star-o.png" alt="starred">';
+    return document.getElementById('chat-channel-name').innerHTML = channelName + ' <small>by <a style="color: white"; href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a></small><img id="star-image" onclick="toggleStar();" src="http://ip.lfe.mw.tum.de/sections/star-o.png" alt="starred">';
 }
 
 function toggleStar() {
-    "use strict";
     console.log('Toggle star empty/full');
     if ($('#star-image').attr('src').indexOf("star-o.png") > -1)
-        $('#star-image').attr('src','http://ip.lfe.mw.tum.de/sections/star.png');
+        return $('#star-image').attr('src','http://ip.lfe.mw.tum.de/sections/star.png');
     else
-        $('#star-image').attr('src','http://ip.lfe.mw.tum.de/sections/star-o.png');
+        return $('#star-image').attr('src','http://ip.lfe.mw.tum.de/sections/star-o.png');
 }
 
 function unselectAll() {
-    "use strict";
     console.log('unselect all');
     $('#new-button').css('color', 'black');
     $('#new-button').css('border-bottom', '0px solid black');
@@ -27,7 +24,6 @@ function unselectAll() {
 }
 
 function selectTab(tabId) {
-    "use strict";
     console.log('Changing to tab');
     unselectAll();
     $(tabId).css('color', '#3F51B5');
@@ -36,7 +32,6 @@ function selectTab(tabId) {
 }
 
 function toggleEmojisBox() {
-    "use strict";
     console.log('Toggle emojis box');
     if ($('#emojis-box').css('visibility').match('hidden'))
         $('#emojis-box').css('visibility','visible');
